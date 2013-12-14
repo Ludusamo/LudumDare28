81cf889c6209fa620c166c8eba43a5cdccff8949
Ludusamo <ludusamo@gmail.com>
2013-12-13 22:29:20 -0500

    Initial Engine

---
+++ Platformer/include/Rock.h
@@ -0,0 +1,15 @@
+#ifndef ROCK_H
+#define ROCK_H
+
+#include "Mob.h"
+
+class Rock : public Mob
+{
+    public:
+        Rock();
+        ~Rock();
+ protected:
+    private:
+};
+
+#endif // ROCK_H
---
+++ Platformer/src/Rock.cpp
@@ -0,0 +1,11 @@
+#include "Rock.h"
+
+Rock::Rock()
+{
+    //ctor
+}
+
+Rock::~Rock()
+{
+    //dtor
+}
