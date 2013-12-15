5fc3751abee1b01879588b642c514b10d3caaef1
alesta <alexstangl777@yahoo.com>
2013-12-15 11:01:41 -0500

    Update Level.h

--- Platformer/include/Level.h
+++ Platformer/include/Level.h
@@ -58,13 +58,10 @@ private:
     Tilemap tmap;
     std::vector<std::vector<int> > colMap;
 
-<<<<<<< HEAD
     void addEntity(Mob* e);
     std::vector<Mob*> entities;
-=======
     KAD kad;
     PPAD ppad;
->>>>>>> origin/master
 
     // Player
     Player player;
