74d1f1885bcb11b3eddadcb925d55f5563703e07
Alexander Stangl <alesta@bergen.org>
2013-12-15 11:43:32 -0500

    Entropy files

---
+++ Platformer/include/Entropy.h
@@ -0,0 +1,27 @@
+#ifndef ENTROPY_H
+#define ENTROPY_H
+
+#include <fstream>
+#include <cmath>
+
+#define FILENAME "res/lvls/entropy.dat"
+
+
+static struct {
+    void write() {
+        std::ofstream out(FILENAME);
+        out << (uint32_t) std::rand() << std::endl;
+        out.close();
+    }
+    uint32_t read() {
+        std::ifstream in(FILENAME);
+        uint32_t seed;
+        in >> seed;
+        in.close();
+        std::srand(seed);
+        write();
+        return std::rand();
+    }
+} Entropy;
+
+#endif // ENTROPY_H
